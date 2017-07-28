d268458988c1bea16b396995d91ca658077e957a
nanthiniravikumar <30317825+nanthiniravikumar@users.noreply.github.com>
2017-07-28 12:15:13 +0530

    Create ww.c

---
+++ ww.c
@@ -0,0 +1,24 @@
+ #include <stdio.h> 
+#define MAX_SIZE 100  
+int main()
+{
+char string[MAX_SIZE];
+    int i, words;
+    printf("Enter any string: ");
+    gets(string); 
+    i = 0;
+    words = 1;
+    while(string[i]!='\0')
+    {
+        if(string[i]==' ' || string[i]=='\n' || string[i]=='\t')
+        {
+            words++;
+        }
+ 
+        i++;
+    }
+ 
+    printf("Total number of words = %d\n", words);
+ 
+    return 0;
+} 
